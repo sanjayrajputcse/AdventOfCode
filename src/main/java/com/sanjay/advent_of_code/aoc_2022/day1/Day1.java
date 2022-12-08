3751e22d1b4acc9e089217be07f0356831eadad6
Sanjay Singh Rajput <sanjay.rajputcse@gmail.com>
2022-12-08 19:11:07 +0000

    day 1 & day 2 puzzle solution

--- src/main/java/com/sanjay/advent_of_code/aoc_2022/day1/Day1.java
+++ src/main/java/com/sanjay/advent_of_code/aoc_2022/day1/Day1.java
@@ -2,15 +2,38 @@ package com.sanjay.advent_of_code.aoc_2022.day1;
 
 import com.sanjay.advent_of_code.InputReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * Question: https://adventofcode.com/2022/day/1
- * Answer : 
+ * Answer :
+ *  Part1: 69177
+ *  Part2:
 */
 public class Day1 {
 
     public static void main(String[] args) throws IOException {
         String[] data = parseInput();
+        int maxCaloriesSoFar = Integer.MIN_VALUE;
+        int totalCalories = 0;
+        List<Integer> totalCaloriesList = new ArrayList<>();
+        for (String line : data) {
+            if(!line.isEmpty()) {
+                totalCalories += Integer.parseInt(line);
+            } else {
+                maxCaloriesSoFar = Math.max(maxCaloriesSoFar, totalCalories);
+                totalCaloriesList.add(totalCalories);
+                totalCalories = 0;
+            }
+        }
+        totalCaloriesList.add(totalCalories);
+        maxCaloriesSoFar = Math.max(maxCaloriesSoFar, totalCalories);
+        System.out.println("Part1: Max Calories => " + maxCaloriesSoFar);
+        totalCaloriesList.sort(Comparator.reverseOrder());
+        System.out.println("Part2: Max 3 Calories sum => " + (totalCaloriesList.get(0)+totalCaloriesList.get(1)+totalCaloriesList.get(2)));
     }
 
     private static String[] parseInput() throws IOException {
--- src/main/java/com/sanjay/advent_of_code/aoc_2022/day2/Day2.java
+++ src/main/java/com/sanjay/advent_of_code/aoc_2022/day2/Day2.java
@@ -5,12 +5,48 @@ import java.io.IOException;
 
 /**
  * Question: https://adventofcode.com/2022/day/2
- * Answer : 
+ * Answer :
+ *  Part1: 12679
 */
 public class Day2 {
 
+    private static final int WIN = 6;
+    private static final int TIE = 3;
+    private static final int LOSE = 0;
+    private static final int[] moveScore = {1, 2, 3};
+    private static final int[] outcomeScore = {LOSE, TIE, WIN};
+    private static final int[][] moveGrid = {
+            {TIE, WIN, LOSE},
+            {LOSE, TIE, WIN},
+            {WIN, LOSE, TIE}
+    };
+
     public static void main(String[] args) throws IOException {
         String[] data = parseInput();
+        int totalScore = 0;
+        for (String line : data) {
+            int opponentMove = line.charAt(0) - 'A';
+            int myMove = line.charAt(2) - 'X';
+            totalScore += moveScore[myMove] + moveGrid[opponentMove][myMove];
+        }
+        System.out.println("Part1: Score => " + totalScore);
+
+        // Part2
+        totalScore = 0;
+        for (String line : data) {
+            int opponentMove = line.charAt(0) - 'A';
+            int outcome = outcomeScore[line.charAt(2) - 'X'];
+            int myMove = 0;
+            for (int i = 0; i <= 2; i++) {
+                if (moveGrid[opponentMove][i] == outcome) {
+                    myMove = i;
+                    break;
+                }
+            }
+
+            totalScore += moveScore[myMove] + moveGrid[opponentMove][myMove];
+        }
+        System.out.println("Part2: Score => " + totalScore);
     }
 
     private static String[] parseInput() throws IOException {
